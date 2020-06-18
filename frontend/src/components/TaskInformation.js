5dc9d94d087dcd9a3c8de56398a0c67926fbc4b6
Anais <akayaturan@gmail.com>
2020-06-18 10:37:06 +0200

    Handle task information editing

---
+++ frontend/src/components/TaskInformation.js
@@ -0,0 +1,12 @@
+class TaskInformation{
+  constructor(id, title, description, completed, status){
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.completed = completed;
+    this.status = status;
+  }
+
+}
+
+export default TaskInformation;
